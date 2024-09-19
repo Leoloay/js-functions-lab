75617b6621ba3b43d7f3744ab7b6a3e866a60934
Loay Al-Mugharghar <almughargharloay@gmail.com>
2024-09-19 22:14:33 +0300

    Done up to Ex

--- app.js
+++ app.js
@@ -120,3 +120,68 @@ const maxOfThree = (num1, num2, num3) => {
 }
 
 console.log("Exercise 6 Result:", maxOfThree(5, 10, 8))
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+const calculateTip = (num4, num5) => {
+  return (num4 * num5) / 100
+}
+
+console.log("Exercise 7 Result:", calculateTip(50, 20))
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+const convertTemperature = (temp, scale) => {
+  if (scale === "C") return `${(temp * 9) / 5 + 32} F`
+  else if (scale === "F") return `${((temp - 32) * 9) / 5} C`
+  else return `Error`
+}
+
+console.log("Exercise 8 Result:", convertTemperature(32, "C"))
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+const basicCalculator = (num6, num7, operator) => {
+  if (operator === "add") return num6 + num7
+  else if (operator === "subtract") return num6 - num7
+  else if (operator === "multiply") return num6 * num7
+  else if (operator === "divide") return num6 / num7
+}
+
+console.log("Exercise 9 Result:", basicCalculator(10, 5, "subtract"))
